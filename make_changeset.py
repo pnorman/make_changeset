3d20f92f9d03de888022ab9fa14c1074e2223996
Paul Norman <penorman@mac.com>
2012-07-15 21:22:37 -0700

    Add initial code that reads the latest state.txt and makes the /api/0.6/changeset/#id call

--- make_changeset.py
+++ make_changeset.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-'''
 
-make_changeset.py beta
+''' make_changeset.py beta
 
 This program builds an osm diff (.osc) file for a changeset by examining minutely diffs
 
@@ -29,3 +28,61 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
 '''
+
+import argparse
+import urllib2
+
+def retry(max_attempts, *exception_types):
+    def tryIt(func):
+        def f(*args, **kwargs):
+            attempts = 0
+            while attempts < max_attempts-1:
+                try:
+                    return func(*args, **kwargs)
+                except exception_types:
+                    attempts += 1
+            return func(*args, **kwargs)
+        return f
+    return tryIt
+
+@retry(5, urllib2.URLError)
+def retry_open(url):
+    return urllib2.urlopen(url)
+
+def isoToTimestamp(isotime):
+    t = datetime.strptime(isotime, "%Y-%m-%dT%H:%M:%SZ")
+    return time.mktime(t.timetuple())
+    
+if __name__ == "__main__":
+    class LineArgumentParser(argparse.ArgumentParser):
+        def convert_arg_line_to_args(self, arg_line):			
+            if arg_line:
+                if arg_line.strip()[0] == '#':
+                    return	
+                for arg in ('--' + arg_line).split():
+                    if not arg.strip():
+                        continue
+                    yield arg
+    parser = LineArgumentParser(description='Create a changeset .osc from replication diffs',fromfile_prefix_chars='@')
+    
+    parser.add_argument('changeset', type=int, help='The changeset to generate')
+    
+    parser.add_argument('--replication-url', help='Base URL of replication diffs', default='http://merry.paulnorman.ca:7201/minute-redaction-replicate/')
+    parser.add_argument('--api-url', help='Base URL of the API', default='http://api.openstreetmap.org/')
+    parser.add_argument('--retry', help='Number of times to retry a failed download', default=5)
+    opts=parser.parse_args()
+    print opts
+    
+     
+    latest_state = retry_open(opts.replication_url + 'state.txt')
+    state={}
+    for line in latest_state:
+        if line[0] == '#':
+                continue
+        (k, v) = line.split('=')
+        state[k] = v.strip().replace("\\:", ":")
+    
+    print state
+    changeset_xml = retry_open(opts.api_url + '/api/0.6/changeset/' + str(opts.changeset))
+
+    print changeset_xml.read()
